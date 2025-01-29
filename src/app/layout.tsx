9502538749ab50bf4c5c6d922a113979910629c4
Ahmadreza <ahmadreza.log@gmail.com>
2025-01-29 16:56:18 +0330

    Fix Some Bugs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next"
 import "../../public/css/globals.css"
-import { ReactNode } from "React"
 import { Poppins } from "next/font/google"
 
 export const metadata: Readonly<Metadata> = {
@@ -15,11 +14,7 @@ const poppins = Poppins( {
 	weight:   [ "100", "200", "300", "400", "500", "600", "700", "800", "900" ],
 } )
 
-interface RootInterface {
-	children: ReactNode
-}
-
-export default function RootLayout( { children }: Readonly<RootInterface> ) {
+export default function RootLayout( { children }: Readonly<{ children: React.ReactNode }> ) {
 	return <html lang="en">
 		<body className={ `bg-slate-900 text-white ${ poppins.className }` }>
 			{ children }
